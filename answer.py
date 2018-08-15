71270914233f61a011960e2caa7851b164e78a07
Bruce Makaaru <brucemakallan@gmail.com>
2018-08-15 16:21:09 +0300

    Build questions API endpoint

---
+++ answer.py
@@ -0,0 +1,8 @@
+class Answer:
+
+    def __init__(self, id, question_id, votes, details, date_posted):
+        self.id = id
+        self.question_id = question_id
+        self.votes = votes
+        self.details = details
+        self.date_posted = date_posted
