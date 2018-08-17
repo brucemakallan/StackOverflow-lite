5c11491108f7c19a43ec430c6215bf49a8d1ce2a
Bruce Makaaru <brucemakallan@gmail.com>
2018-08-17 18:30:17 +0300

    Write unit tests for endpoints

---
+++ tests/test_endpoints.py
@@ -0,0 +1,37 @@
+import unittest
+from flask_api.tests.test_app import app
+
+
+class EndpointsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.test_client
+        self.test_data_question = {
+            "date_posted": "Wed, 01 Aug 2018 00:00:00 GMT",
+            "details": "What is a variable?",
+            "id": 1
+        }
+        self.test_data_answer = {
+            "date_posted": "Fri, 03 Aug 2018 00:00:00 GMT",
+            "details": "A storage construct whose value can change",
+            "id": 1,
+            "question_id": 1,
+            "votes": 12
+        }
+
+    # test for endpoints. Run using $pytest
+    def test_get_all_questions(self):
+        response = self.client().get('/api/v1/questions', data=self.test_data_question)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_question(self):
+        res = self.client().post('/api/v1/questions', data=self.test_data_question)
+        self.assertEqual(res.status_code, 201)
+
+    def test_get_one_question(self):
+        response = self.client().get('/api/v1/questions/1', data=self.test_data_question)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_answer(self):
+        res = self.client().post('/api/v1/questions/1/answers', data=self.test_data_answer)
+        self.assertEqual(res.status_code, 201)
